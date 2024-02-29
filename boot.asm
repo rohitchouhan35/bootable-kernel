9e87fad0c65f1fd47fe4ee9467b22d239fe8c923
rohitchouahn35 <rohitchouhan3512@gmail.com>
2024-02-29 07:28:29 +0530

    feature: get into protected mode

--- boot.asm
+++ boot.asm
@@ -1,5 +1,8 @@
-ORG 0
+ORG 0x7c00
 BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 _start:
     jmp short start
@@ -8,59 +11,70 @@ _start:
 times 33 db 0 ; because some bioses temper data so we are just creating fake one
 
 start:
-    jmp 0x7c0:step2
+    jmp 0:step2
 
 step2:
     cli ; Clear Interrupts
-    mov ax, 0x7c0
+    mov ax, 0x00
     mov ds, ax
     mov es, ax
-    mov ax, 0x00
     mov ss, ax
     mov sp, 0x7c00
+    sti ; ENables interrupts
 
-    ; setting registers so the interrupt 13 can read from hard disk
-    mov ah, 2 ; read sector command
-    mov al, 1 ; one sector to read
-    mov ch, 0 ; cylinder low eight bits
-    mov cl, 2 ; read secotow two
-    mov dh, 0 ; head number
-    mov bx, buffer
-    int 0x13
-    jc error
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
 
-    mov si, buffer
-    call print
+; GDT
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0 ; 64 bits set to 0
+
+; offset 0x8
+gdt_code:     ; CS SHOULD POINT TO THIS
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x9a   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
+
+; offset 0x10
+gdt_data:      ; DS, SS, ES, FS, GS
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x92   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start-1
+    dd gdt_start
+ 
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
     jmp $
-
-error:
-    mov si, error_message
-    call print
-    jmp $
-
-print:
-    mov bx, 0
-.loop:
-    lodsb
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-
-.done:
-    ret
-
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
-
-error_message: db 'Failed to load sector', 0
 
 ; we need to fill atleast 510 bytes of data
 ; so basically it's like padding with 0 to fill to the 510 bytes
 times 510-($ - $$) db 0
 ; put this signature word
 dw 0xAA55 
-
-buffer:
+ 
